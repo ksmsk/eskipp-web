460e7df65a06ec89be01636821b65076617eab7a
Kubilen Simsek <simsek@kubilen.com>
2021-02-07 01:05:07 +0300

    adds new helper functions

--- shared/utils/helpers.ts
+++ shared/utils/helpers.ts
@@ -1,2 +1,29 @@
+export const currentYear = new Date().getFullYear();
+
 export const expireDate = (seconds: number) =>
   new Date(new Date().getTime() + seconds * 1000);
+
+export const debounce = (func: Function, wait: number) => {
+  let timeout: any;
+
+  return function executedFunction(...args: any) {
+    const later = () => {
+      timeout = null;
+      func(...args);
+    };
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(later, wait);
+  };
+};
+
+export const linkShortener = (url: string) => {
+  if (!url.startsWith("http")) {
+    return url;
+  }
+  if (url.length > 30) {
+    return url.slice(0, 30) + "...";
+  }
+  return url;
+};
