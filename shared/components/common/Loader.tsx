ea4c5a5f1ade06916a7cb1ddc9308ade45f62401
Kubilen Simsek <simsek@kubilen.com>
2021-02-07 01:12:41 +0300

    implements loader component

--- shared/components/common/Loader.tsx
+++ shared/components/common/Loader.tsx
@@ -6,8 +6,8 @@ type Props = {};
 export const Loader: React.FC<Props> = () => {
   return (
     <div
-      className="flex items-center justify-center"
-      style={{ height: "calc(100vh - 85px)" }}
+      className="flex items-center justify-center overflow-hidden"
+      style={{ height: "calc(100vh - 94px)" }}
     >
       <Spinner />
     </div>
